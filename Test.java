40eb54020d2341d637bf532571e0d2b5c096e75c
Sarah Green <sjaissergreen@gmail.com>
2016-09-19 00:09:06 -0700

    Initiated repo

---
+++ Test.java
@@ -0,0 +1,18 @@
+/************************************************
+* Test.java
+* Tests the Game class for a game of video poker
+* Written by Sarah Green (stg2117)
+***********************************************/
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// create a new game
+		Game myGame = new Game();
+
+		// play the game
+		myGame.playGame();
+
+	}
+}
